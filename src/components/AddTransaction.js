ab6d129105f398e8ed73c800687bced102201103
MuhammadHassan99 <hassanmuhamamd436@gmail.com>
2021-06-05 16:51:55 +0500

    commit

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,4 +1,5 @@
 import React, { useState, useContext } from "react";
+import { Formik, Field, Form } from "formik";
 
 // Import the Global State
 import { GlobalContext } from "../context/GlobalState";
@@ -9,9 +10,7 @@ const AddTransaction = () => {
 
   const { addTransaction } = useContext(GlobalContext);
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-
+  const action = () => {
     const newTransaction = {
       id: new Date().getTime(),
       description,
@@ -19,36 +18,48 @@ const AddTransaction = () => {
     };
 
     addTransaction(newTransaction);
+    setDescription("");
+    setTransactionAmount("");
   };
 
   return (
     <div>
       <h3>Add New Transaction</h3>
-      <form onSubmit={onSubmit}>
-        <div className='form-control'>
-          <label htmlFor='description'>Description</label>
-          <input
-            type='text'
-            id='description'
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            placeholder='Detail of Transaction'
-            required='required'
-          />
-        </div>
-        <div className='form-control'>
-          <label htmlFor='transactionamount'>Transaction Amount</label>
-          <input
-            type='number'
-            id='transactionamount'
-            value={transactionAmount}
-            onChange={(e) => setTransactionAmount(e.target.value)}
-            placeholder='Dollar Value of Transaction'
-            required='required'
-          />
-        </div>
-        <button className='btn'>Add Transaction</button>
-      </form>
+      <Formik
+        initialValues={{
+          description: "",
+          amount: "",
+        }}
+        onSubmit={action}
+      >
+        <Form>
+          <div className='form-control'>
+            <label htmlFor='description'>Description</label>
+            <Field
+              type='text'
+              id='description'
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              placeholder='Detail of Transaction'
+              required='required'
+            />
+          </div>
+          <div className='form-control'>
+            <label htmlFor='transactionamount'>Transaction Amount</label>
+            <Field
+              type='number'
+              id='transactionamount'
+              value={transactionAmount}
+              onChange={(e) => setTransactionAmount(e.target.value)}
+              placeholder='Dollar Value of Transaction'
+              required='required'
+            />
+          </div>
+          <button className='btn' type='submit'>
+            Add Transaction
+          </button>
+        </Form>
+      </Formik>
     </div>
   );
 };
